6a34a028900073065c56d3a2f13ddb1efdf25d39
JoEunSang <sang11032@gmail.com>
2024-04-14 17:09:18 +0900

    [feat] connect server

--- src/pages/LogPage/LogForm.js
+++ src/pages/LogPage/LogForm.js
@@ -1,28 +1,21 @@
 import Sidebar from "../MainPage/Sidebar";
-import React from "react";
+import React, { useEffect } from "react";
 import LogCard from "./LogCard";
 import './LogForm.css';
-import EmergencyAlert from "../NoticePage/EmergencyAlert";
+import {useLocation} from "react-router";
+
 
 const LogForm = () => {
-    const logs = [
-        { id: 1, log: '입실', time: '11:30:19', status: 'normal' },
-        { id: 2, log: '앉음', time: '11:45:00', status: 'normal' },
-        { id: 3, log: '낙상 발생', time: '12:15:22', status: 'emergency' },
-        { id: 4, log: '누움', time: '13:30:45', status: 'normal' },
-        { id: 5, log: '서 있음', time: '14:00:12', status: 'normal' },
-        { id: 6, log: '퇴실', time: '15:10:30', status: 'normal' },
-        { id: 7, log: '입실', time: '16:00:00', status: 'normal' },
-    ];
-
+    const {state} = useLocation()
+    console.log(state);
+    const logs = Array.isArray(state?.logs) ? state.logs : [];
     return (
         <div className="log-form-layout">
             <Sidebar />
-
             <div className="log-form">
                 <div className="room-info">
                     <p className="log-room-num">B 102</p>
-                    <p className="patient-name">조은상 님</p>
+                    <p className="patient-name">{state.name}</p>
                 </div>
                 <div className="log-list">
                     {logs.map(log => (
