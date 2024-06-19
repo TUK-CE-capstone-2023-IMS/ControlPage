5aa658b188843271cc713758765f1b069e1039fe
JoEunSang <sang11032@gmail.com>
2024-06-19 19:27:51 +0900

    [ui] 로그인, 회원가입 ui 수정

--- src/pages/AuthPage/SignIn.js
+++ src/pages/AuthPage/SignIn.js
@@ -47,123 +47,134 @@ const SignupPage = () => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <div className="signup_page">
-                <div className="signup_box">
-                    <p className="signup_title">SignIn</p>
+            <div className="signup_wrap">
 
-                    <div className="signup_input_box">
-                        <div className="signup_id">
-                            <input
-                                type="text"
-                                name="managerid"
-                                value={formData.managerid}
-                                onChange={handleChange}
-                                placeholder="아이디"
-                                id="BsPerson"
-                                required
-                            />
+                <div className="signup_page">
+
+                    <div className="signup_wrap_box">
+
+                        <div className="signup_box">
+                            <div className="signup_total_box">
+                                <div className="signup_title">SignIn</div>
+
+                                <div className="signup_input_box">
+                                    <div className="signup_id">
+                                        <input
+                                            type="text"
+                                            name="managerid"
+                                            value={formData.managerid}
+                                            onChange={handleChange}
+                                            placeholder="아이디"
+                                            id="BsPerson"
+                                            required
+                                        />
+                                    </div>
+
+                                    <div className="signup_password_input">
+                                        {/*<img src={lock} width="24px" height="24px" className="lockIcon"/>*/}
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            value={formData.password}
+                                            onChange={handleChange}
+                                            placeholder="비밀번호"
+                                            required
+                                        />
+                                    </div>
+
+                                    <div className="signup_email">
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            value={formData.email}
+                                            onChange={handleChange}
+                                            placeholder="이메일"
+                                            required
+                                        />
+                                    </div>
+
+                                    <div className="signup_name">
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            value={formData.name}
+                                            onChange={handleChange}
+                                            placeholder="성명"
+                                            required
+                                        />
+                                    </div>
+
+                                    <div className="signup_sex">
+                                        <select
+                                            name="sex"
+                                            value={formData.sex}
+                                            onChange={handleChange}
+                                            required
+                                        >
+                                            <option value="">성별 선택</option>
+                                            <option value="male">남자</option>
+                                            <option value="female">여자</option>
+                                        </select>
+                                    </div>
+
+                                    <div className="signup_phone_number">
+                                        <input
+                                            type="text"
+                                            name="phone"
+                                            value={formData.phone}
+                                            onChange={handleChange}
+                                            placeholder="전화번호"
+                                            required
+                                        />
+                                    </div>
+
+                                    <div className="signup_age">
+                                        <input
+                                            type="number"
+                                            name="age"
+                                            value={formData.age}
+                                            onChange={handleChange}
+                                            placeholder="나이"
+                                            required
+                                        />
+                                    </div>
+
+                                    <div className="signup_address">
+                                        <input
+                                            type="text"
+                                            name="address"
+                                            value={formData.address}
+                                            onChange={handleChange}
+                                            placeholder="주소"
+                                            required
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className= "signup_button">
+                                    <button className="signup_button" type="submit" onClick={handleSignIn}>회원가입</button>
+                                </div>
+                            </div>
                         </div>
 
-                        <div className="signup_password_input">
-                            {/*<img src={lock} width="24px" height="24px" className="lockIcon"/>*/}
-                            <input
-                                type="password"
-                                name="password"
-                                value={formData.password}
-                                onChange={handleChange}
-                                placeholder="비밀번호"
-                                required
-                            />
-                        </div>
-
-                        <div className="signup_email">
-                            <input
-                                type="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                placeholder="이메일"
-                                required
-                            />
-                        </div>
-
-                        <div className="signup_name">
-                            <input
-                                type="text"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                placeholder="성명"
-                                required
-                            />
-                        </div>
-
-                        <div className="signup_sex">
-                            <select
-                                name="sex"
-                                value={formData.sex}
-                                onChange={handleChange}
-                                required
-                            >
-                                <option value="">성별 선택</option>
-                                <option value="male">남자</option>
-                                <option value="female">여자</option>
-                            </select>
-                        </div>
-
-                        <div className="signup_phone_number">
-                            <input
-                                type="text"
-                                name="phone"
-                                value={formData.phone}
-                                onChange={handleChange}
-                                placeholder="전화번호"
-                                required
-                            />
-                        </div>
-
-                        <div className="signup_age">
-                            <input
-                                type="number"
-                                name="age"
-                                value={formData.age}
-                                onChange={handleChange}
-                                placeholder="나이"
-                                required
-                            />
-                        </div>
-
-                        <div className="signup_address">
-                            <input
-                                type="text"
-                                name="address"
-                                value={formData.address}
-                                onChange={handleChange}
-                                placeholder="주소"
-                                required
-                            />
-                        </div>
                     </div>
 
-                    <div className= "signup_button">
-                        <button className="signup_button" type="submit" onClick={handleSignIn}>회원가입</button>
-                    </div>
-                </div>
-
-                <div className="signup_hello_box">
-                    <div className="signup_hello_text">
-                        <div className="signup_first_hello_box">
-                            <div className="signup_hello_text_1">환영합니다!</div>
-                            <div className="signup_hello_text_2">대시보드를 통해 환자 모니터링 및 관리를 시작해보세요!</div>
-                        </div>
-                        <div className="signup_second_hello_box">
-                            <div className="signup_hello_text_3">이미 계정이 있으신가요?</div>
-                            <div className="signup_hello_text_4">
-                                <a href="/login">로그인 하러가기</a>
+                    <div className="signup_hello_box">
+                        <div className="signup_hello_text">
+                            <div className="signup_first_hello_box">
+                                <div className="signup_hello_text_1">환영합니다!</div>
+                                <div className="signup_hello_text_2">대시보드를 통해 환자 모니터링 및 관리를 시작해보세요!</div>
+                            </div>
+                            <div className="signup_second_hello_box">
+                                <div className="signup_hello_text_3">이미 계정이 있으신가요?</div>
+                                <div className="signup_hello_text_4">
+                                    <a href="/login">로그인 하러가기</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
         </form>
     );
